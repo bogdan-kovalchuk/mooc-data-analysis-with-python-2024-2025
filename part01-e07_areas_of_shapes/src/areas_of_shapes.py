ace78e72577ce253f2687d83eccd9a74078f6484
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-04-30 21:15:00 +0300

    Part01-e07 solution

--- part01-e07_areas_of_shapes/src/areas_of_shapes.py
+++ part01-e07_areas_of_shapes/src/areas_of_shapes.py
@@ -4,7 +4,25 @@ import math
 
 
 def main():
-    # enter you solution here
+    while True:
+        shape = input("Choose a shape (triangle, rectangle, circle): ")
+        if shape == "triangle":
+            base = int(input("Give base of the triangle: "))
+            height = int(input("Give height of the triangle: "))
+            print(f"The area is {0.5*base*height:1.6f}")
+        elif shape == "rectangle":
+            width = int(input("Give width of the rectangle: "))
+            height = int(input("Give height of the rectangle: "))
+            print(f"The area is {width*height:1.6f}")
+        elif shape == "circle":
+            radius = int(input("Give radius of the circle:"))
+            print(f"The area is {math.pi*radius*radius:1.6f}")
+        elif shape == "":
+            break
+        else:
+            print("Unknown shape!")
+            continue
+
 
 if __name__ == "__main__":
     main()
