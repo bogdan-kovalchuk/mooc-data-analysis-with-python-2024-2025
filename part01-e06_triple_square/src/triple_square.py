79a0b3f6497461c832100bf1f9a604ab19b32f68
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-04-30 20:53:32 +0300

    Part01-e6 solution

--- part01-e06_triple_square/src/triple_square.py
+++ part01-e06_triple_square/src/triple_square.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 
 
+def triple(x: int):
+    return 3 * x
+
+
+def square(x: int):
+    return x * x
+
+
 def main():
-    pass
+    for i in range(1, 11):
+        tr = triple(i)
+        sq = square(i)
+        if sq > tr:
+            break
+        print(f"triple({i})=={tr} square({i})=={sq}")
+
 
 if __name__ == "__main__":
     main()
