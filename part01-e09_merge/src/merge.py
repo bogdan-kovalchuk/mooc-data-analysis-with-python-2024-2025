3f52999309b5c3d7caadde02c18896a6cf557ca1
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-01 15:56:03 +0300

    Part01-e09 solution

--- part01-e09_merge/src/merge.py
+++ part01-e09_merge/src/merge.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
 
+
 def merge(L1, L2):
-    return []
+    result = []
+    i = j = 0
+    while i < len(L1) and j < len(L2):
+        if L1[i] < L2[j]:
+            result.append(L1[i])
+            i += 1
+        else:
+            result.append(L2[j])
+            j += 1
+    result.extend(L1[i:])
+    result.extend(L2[j:])
+    return result
+
 
 def main():
-    pass
+    L1 = [1, 5, 9, 12]
+    L2 = [2, 6, 10]
+    print(merge(L1, L2))
+
 
 if __name__ == "__main__":
     main()
