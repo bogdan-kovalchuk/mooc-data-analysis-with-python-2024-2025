5f869678df70e76e3f8a49e1f9dd9e608b9e4180
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-02 14:29:45 +0300

    Part01-e11 solution

--- part01-e11_interleave/src/interleave.py
+++ part01-e11_interleave/src/interleave.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
+
 def interleave(*lists):
-    return []
+    out = []
+    for lst in zip(*lists):
+        out.extend(lst)
+    return out
+
 
 def main():
-    print(interleave([1, 2, 3], [20, 30, 40], ['a', 'b', 'c']))
+    print(interleave([1, 2, 3], [20, 30, 40], ["a", "b", "c"]))
+
 
 if __name__ == "__main__":
     main()
