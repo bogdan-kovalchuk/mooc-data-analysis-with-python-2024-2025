a350e4c19129788bc8a5dfbf3ab7ab63fd72c965
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-02 16:29:32 +0300

    Part01-e12 solution

--- part01-e12_distinct_characters/src/distinct_characters.py
+++ part01-e12_distinct_characters/src/distinct_characters.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
+
 def distinct_characters(L):
-    return {}
+    return {word: len(set(word)) for word in L}
+
 
 def main():
     print(distinct_characters(["check", "look", "try", "pop"]))
+
 
 if __name__ == "__main__":
     main()
