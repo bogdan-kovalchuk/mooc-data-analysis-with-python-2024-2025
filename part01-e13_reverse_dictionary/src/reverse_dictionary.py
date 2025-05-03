cb5e9f098072325d3b83520d71be66a9e348fc9b
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-03 09:16:19 +0300

    Part01-e13 solution

--- part01-e13_reverse_dictionary/src/reverse_dictionary.py
+++ part01-e13_reverse_dictionary/src/reverse_dictionary.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python3
 
+
 def reverse_dictionary(d):
-    return {}
+    result = {}
+    for english, finnish_lst in d.items():
+        for finnish in finnish_lst:
+            if finnish not in result:
+                result[finnish] = [english]
+            else:
+                result[finnish].append(english)
+    return result
+
 
 def main():
-    pass
+    d = {
+        "move": ["liikuttaa"],
+        "hide": ["piilottaa", "salata"],
+        "six": ["kuusi"],
+        "fir": ["kuusi"],
+    }
+
+    print(reverse_dictionary(d))
+
 
 if __name__ == "__main__":
     main()
