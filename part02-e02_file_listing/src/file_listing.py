fdbb32d971af416110cff1f6f0462b049f2b0775
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-10 16:04:27 +0300

    Part02-e02 solution

--- part02-e02_file_listing/src/file_listing.py
+++ part02-e02_file_listing/src/file_listing.py
@@ -4,10 +4,18 @@ import re
 
 
 def file_listing(filename="src/listing.txt"):
-    return []
+    result = []
+    with open(filename) as f:
+        for l in f:
+            match = re.search(r"(\d+)\s+(\w+)\s+(\d+)\s+(\d+):(\d+)\s+([\w.]+)", l[30:])
+            t = match.groups()
+            result.append((int(t[0]), t[1], int(t[2]), int(t[3]), int(t[4]), t[5]))
+    return result
+
 
 def main():
-    pass
+    print(file_listing())
+
 
 if __name__ == "__main__":
     main()
