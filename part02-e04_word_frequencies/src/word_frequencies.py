1a8682cdcdd7fd991548798133e58ee34a6fe846
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-11 18:40:07 +0300

    Part02-e04 solution

--- part02-e04_word_frequencies/src/word_frequencies.py
+++ part02-e04_word_frequencies/src/word_frequencies.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 
-def word_frequencies(filename):
-    return {}
+
+def word_frequencies(filename="src/alice.txt"):
+    chars = """!"#$%&'()*,-./:;?@[]_"""
+    freq = {}
+    with open(filename) as f:
+        for line in f:
+            for word in line.split():
+                cleaned = word.strip(chars)
+                freq[cleaned] = freq.get(cleaned, 0) + 1
+    return freq
+
 
 def main():
-    pass
+    print(word_frequencies())
+
 
 if __name__ == "__main__":
     main()
