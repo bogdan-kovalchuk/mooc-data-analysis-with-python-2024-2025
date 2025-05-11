e36488bf9bd7cda10e23a0eca84328805019d4a9
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-11 11:35:04 +0300

    Part02-e03 solution

--- part02-e03_red_green_blue/src/red_green_blue.py
+++ part02-e03_red_green_blue/src/red_green_blue.py
@@ -2,12 +2,20 @@
 
 import re
 
+
 def red_green_blue(filename="src/rgb.txt"):
-    return []
+    clead_lines = []
+    with open(filename) as f:
+        f.readline()
+        for line in f:
+            groups = re.search(r"(\d+)\s+(\d+)\s+(\d+)\s+(.+)", line).groups()
+            clead_lines.append("\t".join(groups))
+    return clead_lines
 
 
 def main():
-    pass
+    print(red_green_blue())
+
 
 if __name__ == "__main__":
     main()
