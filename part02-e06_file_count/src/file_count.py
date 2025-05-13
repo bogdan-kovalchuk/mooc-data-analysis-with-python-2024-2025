8a2063549c89b859b13137c7ce0420cc395ad768
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-13 20:31:32 +0300

    Part02-e06 solution

--- part02-e06_file_count/src/file_count.py
+++ part02-e06_file_count/src/file_count.py
@@ -2,11 +2,24 @@
 
 import sys
 
+
 def file_count(filename):
-    return (0, 0, 0)
+    linecount = 0
+    wordcount = 0
+    charactercount = 0
+    with open(filename) as f:
+        for line in f:
+            linecount += 1
+            wordcount += len(line.split())
+            charactercount += len(line)
+    return (linecount, wordcount, charactercount)
+
 
 def main():
-    pass
+    for filename in sys.argv[1:]:
+        linecount, wordcount, charactercount = file_count(filename)
+        print(f"{linecount}\t{wordcount}\t{charactercount}\t{filename}")
+
 
 if __name__ == "__main__":
     main()
