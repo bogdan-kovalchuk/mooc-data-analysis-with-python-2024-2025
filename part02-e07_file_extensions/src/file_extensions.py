e5d527c10a4b0c01e612983d548aeaeaac41f1d0
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-14 11:48:56 +0300

    Part02-e07 solution

--- part02-e07_file_extensions/src/file_extensions.py
+++ part02-e07_file_extensions/src/file_extensions.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python3
 
+
 def file_extensions(filename):
-    return ([], {})
+    no_extensions = []
+    with_extensions = {}
+
+    with open(filename) as f:
+        for line in f:
+            file = line.strip()
+            name, extension = (file.rsplit(".", 1) + [None])[:2]
+
+            if extension:
+                with_extensions.setdefault(extension, []).append(file)
+            else:
+                no_extensions.append(file)
+
+    return no_extensions, with_extensions
+
 
 def main():
-    pass
+    no_extensions, with_extensions = file_extensions("src/filenames.txt")
+    print(f"{len(no_extensions)} files with no extension")
+    for k, v in with_extensions.items():
+        print(f"{k} {len(v)}")
+
 
 if __name__ == "__main__":
     main()
