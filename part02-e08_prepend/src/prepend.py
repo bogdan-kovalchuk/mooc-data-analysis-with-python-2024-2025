505ec7d36ad652a31519bd69c6d80afa7667fada
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-16 17:32:35 +0300

    Part02-e08 solution

--- part02-e08_prepend/src/prepend.py
+++ part02-e08_prepend/src/prepend.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
+
 class Prepend(object):
-    # Add the methods of the class here
+    def __init__(self, start: str):
+        self.start = start
+
+    def write(self, s: str):
+        print(f"{self.start}{s}")
+
 
 def main():
-    pass
+    p = Prepend("+++ ")
+    p.write("Hello")
+
 
 if __name__ == "__main__":
     main()
