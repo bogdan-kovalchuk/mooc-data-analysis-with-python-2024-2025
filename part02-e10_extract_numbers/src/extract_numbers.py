1ed59c9769b1d4c8a985cf156271b494090fc4c0
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-20 09:38:32 +0300

    Part02-e10 solution

--- part02-e10_extract_numbers/src/extract_numbers.py
+++ part02-e10_extract_numbers/src/extract_numbers.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
 
+
 def extract_numbers(s):
-    return []
+    words = s.split()
+    numbers = []
+    for word in words:
+        try:
+            num = int(word)
+            numbers.append(num)
+        except Exception as e:
+            try:
+                num = float(word)
+                numbers.append(num)
+            except Exception as e:
+                continue
+    return numbers
+
 
 def main():
     print(extract_numbers("abd 123 1.2 test 13.2 -1"))
+
 
 if __name__ == "__main__":
     main()
