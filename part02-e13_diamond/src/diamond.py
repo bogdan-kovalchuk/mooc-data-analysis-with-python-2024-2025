6130063345895e52d5d90a7067182afc5af57aac
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-21 21:21:47 +0300

    Part02-e13 solution

--- part02-e13_diamond/src/diamond.py
+++ part02-e13_diamond/src/diamond.py
@@ -2,11 +2,19 @@
 
 import numpy as np
 
+
 def diamond(n):
-    return np.array([])
+    eye = np.eye(n, dtype=int)
+    flipped = np.flipud(eye[:, :-1])
+    top = np.concatenate((flipped, eye), axis=1)
+    bottom = np.flipud(top)[1:, :]
+    full = np.concatenate((top, bottom), axis=0)
+    return full
+
 
 def main():
-    pass
+    print(diamond(4))
+
 
 if __name__ == "__main__":
     main()
