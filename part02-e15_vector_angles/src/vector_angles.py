6905b20b38602b986c3c73cf6ee780b80a15a4f8
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-22 16:21:25 +0300

    Part02-e15 solution

--- part02-e15_vector_angles/src/vector_angles.py
+++ part02-e15_vector_angles/src/vector_angles.py
@@ -3,11 +3,21 @@
 import numpy as np
 import scipy.linalg
 
+
 def vector_angles(X, Y):
-    np.array([])
+    dot_prod = np.sum(X * Y, axis=1)
+    norm_X = np.sqrt(np.sum(X**2, axis=1))
+    norm_Y = np.sqrt(np.sum(Y**2, axis=1))
+    cos_theta = np.clip(dot_prod / (norm_X * norm_Y), -1, 1)
+    angles_rad = np.arccos(cos_theta)
+    return np.degrees(angles_rad)
+
 
 def main():
-    np.array([])
+    X = np.array([[1, 2, 4], [5, 6, 2]])
+    Y = np.array([[3, 4, 1], [4, 5, 2]])
+    print(vector_angles(X, Y))
+
 
 if __name__ == "__main__":
     main()
