9cf33d5b40de19154137e90fe162874b59cc4eb9
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-25 21:36:02 +0300

    Part03-e04 solution

--- part03-e04_matrix_power/src/matrix_power.py
+++ part03-e04_matrix_power/src/matrix_power.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
 
+import numpy as np
+from functools import reduce
+
+
+from functools import reduce
+import numpy as np
+
 
 def matrix_power(a, n):
-    np.eye(2)
+    if n < 0:
+        a = np.linalg.inv(a)
+    return reduce(
+        np.matmul, (a for _ in range(abs(n))), np.eye(a.shape[0], dtype=a.dtype)
+    )
+
 
 def main():
-    return
+    print(matrix_power(np.array([[1, 2], [3, 4]]), 3))
+
 
 if __name__ == "__main__":
     main()
