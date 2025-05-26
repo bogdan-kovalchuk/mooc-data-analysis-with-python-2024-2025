f8d329441b3eecee96c3b444f1c0765751c6397d
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-26 17:58:40 +0300

    Part03-e10 solution

--- part03-e10_subfigures/src/subfigures.py
+++ part03-e10_subfigures/src/subfigures.py
@@ -3,11 +3,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def subfigures(a):
-    pass
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.plot(a[:, 0], a[:, 1])
+    ax2.scatter(a[:, 0], a[:, 1], c=a[:, 2], s=a[:, 3])
+    plt.show()
+
 
 def main():
-    pass
+    n = 10
+    a = np.random.randint(0, 10, (n, 3))
+    a = np.concatenate([np.arange(n)[:, np.newaxis], a], axis=1)
+    subfigures(a)
+
 
 if __name__ == "__main__":
     main()
