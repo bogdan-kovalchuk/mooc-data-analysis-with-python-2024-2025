ba3ed4b5c54364b33a242deb6f9f300b2d99b5ea
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-26 07:58:54 +0300

    Part03-e05 solution

--- part03-e05_correlation/src/correlation.py
+++ part03-e05_correlation/src/correlation.py
@@ -3,19 +3,27 @@
 import scipy.stats
 import numpy as np
 
+
 def load():
     import pandas as pd
-    return pd.read_csv("src/iris.csv").drop('species', axis=1).values
+
+    return pd.read_csv("src/iris.csv").drop("species", axis=1).values
+
 
 def lengths():
-    return 0
+    iris = load()
+    return scipy.stats.pearsonr(iris[:, 0], iris[:, 2])[0]
+
 
 def correlations():
-    return np.array([])
+    iris = load()
+    return np.corrcoef(iris, rowvar=False)
+
 
 def main():
     print(lengths())
     print(correlations())
+
 
 if __name__ == "__main__":
     main()
