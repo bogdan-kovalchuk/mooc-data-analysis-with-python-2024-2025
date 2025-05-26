9fc163ef19eed2ac5b0a2538b0b421fa36206bc0
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-26 15:05:00 +0300

    Part03-e09 solution

--- part03-e09_multiple_graphs/src/multiple_graphs.py
+++ part03-e09_multiple_graphs/src/multiple_graphs.py
@@ -2,8 +2,15 @@
 
 import matplotlib.pyplot as plt
 
+
 def main():
-    pass
+    plt.plot([2, 4, 6, 7], [4, 3, 5, 1])
+    plt.plot([1, 2, 3, 4], [4, 2, 3, 1])
+    plt.title("Exercise 3.9 (multiple graphs)")
+    plt.xlabel("x-axis")
+    plt.ylabel("y-axis")
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
