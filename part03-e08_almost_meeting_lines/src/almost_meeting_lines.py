5ca18005e7e2eff994dfb2711da462f2e7f09e5d
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-26 14:17:51 +0300

    Part03-e08 solution

--- part03-e08_almost_meeting_lines/src/almost_meeting_lines.py
+++ part03-e08_almost_meeting_lines/src/almost_meeting_lines.py
@@ -2,45 +2,56 @@
 
 import numpy as np
 
+
 def almost_meeting_lines(a1, b1, a2, b2):
-    return []
+    A = np.array([[-a1, 1], [-a2, 1]])
+    b = np.array([b1, b2])
+    exact = True
+    try:
+        x, y = np.linalg.solve(A, b)
+    except:
+        x, y = np.linalg.lstsq(A, b, rcond=None)[0]
+        exact = False
+    return (x, y), bool(exact)
+
 
 def main():
-    a1=1
-    b1=2
-    a2=-1
-    b2=0
+    a1 = 1
+    b1 = 2
+    a2 = -1
+    b2 = 0
 
     (x, y), exact = almost_meeting_lines(a1, b1, a2, b2)
     if exact:
         print(f"Lines meet at x={x} and y={y}")
 
-    a1=a2=1
-    b1=2
-    b2=-2
+    a1 = a2 = 1
+    b1 = 2
+    b2 = -2
     (x, y), exact = almost_meeting_lines(a1, b1, a1, b2)
     if exact:
         print(f"Lines meet at x={x} and y={y}")
     else:
         print(f"Closest point at x={x} and y={y}")
 
-    a1=1
-    b1=2
+    a1 = 1
+    b1 = 2
     (x, y), exact = almost_meeting_lines(a1, b1, a1, b1)
     if exact:
         print(f"Lines meet at x={x} and y={y}")
     else:
         print(f"Closest point at x={x} and y={y}")
 
-    a1=1
-    b1=2
-    a2=1
-    b2=1
+    a1 = 1
+    b1 = 2
+    a2 = 1
+    b2 = 1
     (x, y), exact = almost_meeting_lines(a1, b1, a2, b2)
     if exact:
         print(f"Lines meet at x={x} and y={y}")
     else:
         print(f"Closest point at x={x} and y={y}")
+
 
 if __name__ == "__main__":
     main()
