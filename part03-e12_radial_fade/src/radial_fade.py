df912497446cf56c0b41811f27659a46b769d961
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-27 11:23:58 +0300

    Part03-e12 solution

--- part03-e12_radial_fade/src/radial_fade.py
+++ part03-e12_radial_fade/src/radial_fade.py
@@ -3,25 +3,49 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def center(a):
-    return (0,0)   # note the order: (center_y, center_x)
-
-def radial_distance(a):
-    return np.array([[]])
 
 def scale(a, tmin=0.0, tmax=1.0):
-    """Returns a copy of array 'a' with its values scaled to be in the range
-[tmin,tmax]."""
-    return np.array([[]])
+    """Returns a copy of array 'a' with its values scaled to be in the range [tmin,tmax]."""
+    a_min = np.min(a)
+    a_max = np.max(a)
+    if a_min == a_max:
+        return np.zeros_like(a, dtype=float)
+    return (a - a_min) / (a_max - a_min) * (tmax - tmin) + tmin
+
+
+def center(a):
+    h, w = a.shape[:2]
+    return (h - 1) / 2, (w - 1) / 2
+
+
+def radial_distance(a):
+    y_idx, x_idx = np.indices(a.shape[:2])
+    center_y, center_x = center(a)
+    return np.sqrt((y_idx - center_y) ** 2 + (x_idx - center_x) ** 2)
+
 
 def radial_mask(a):
-    return np.array([[]])
+    dist = radial_distance(a)
+    dist_scaled = scale(dist)
+    return 1.0 - dist_scaled
+
 
 def radial_fade(a):
-    return np.array([[]])
+    return a * radial_mask(a)[:, :, np.newaxis]
+
 
 def main():
-    pass
+    painting = plt.imread("src/painting.png")
+
+    print(center(painting))
+    print(radial_distance(painting))
+
+    fig, ax = plt.subplots(3, 1)
+    ax[0].imshow(painting)
+    ax[1].imshow(radial_mask(painting))
+    ax[2].imshow(radial_fade(painting))
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
