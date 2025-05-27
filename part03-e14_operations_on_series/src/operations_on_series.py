cf16470c126ca707de47a6b41789a78eb6b30191
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-27 21:39:04 +0300

    Part03-e14 solution

--- part03-e14_operations_on_series/src/operations_on_series.py
+++ part03-e14_operations_on_series/src/operations_on_series.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3
 
+import pandas as pd
+import numpy as np
+
 
 def create_series(L1, L2):
-    return (None, None)
-    
+    s1 = pd.Series(L1, index=["a", "b", "c"])
+    s2 = pd.Series(L2, index=["a", "b", "c"])
+    return s1, s2
+
+
 def modify_series(s1, s2):
-    return (None, None)
-    
+    s1["d"] = s2["b"]
+    s2.drop("b", inplace=True)
+    return s1, s2
+
+
 def main():
-    return
-    
+    L1 = np.array([1, 3, 5])
+    L2 = np.array([2, 4, 8])
+    s1, s2 = create_series(L1, L2)
+    s1, s2 = modify_series(s1, s2)
+    s = s1 + s2
+    print(s)
+
+
 if __name__ == "__main__":
     main()
