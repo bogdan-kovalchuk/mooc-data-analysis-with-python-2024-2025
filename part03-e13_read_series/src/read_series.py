9aec207f1a63d2b28294cdda7d02a464f2bddc2e
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-27 19:04:02 +0300

    Part03-e13 solution

--- part03-e13_read_series/src/read_series.py
+++ part03-e13_read_series/src/read_series.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
+import pandas as pd
 
 
 def read_series():
-    return pd.Series()
+    result = pd.Series([])
+    while True:
+        user_input = input()
+        if not user_input:
+            break
+        idx, val = user_input.split()
+        result[idx] = val
+    return result
+
 
 def main():
-    pass
+    print(read_series())
+
 
 if __name__ == "__main__":
     main()
