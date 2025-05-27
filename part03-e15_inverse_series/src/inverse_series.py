d64ad84eb0b468f6bbfabd8285d2f162fe3dfe17
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-27 21:46:53 +0300

    Part03-e15 solution

--- part03-e15_inverse_series/src/inverse_series.py
+++ part03-e15_inverse_series/src/inverse_series.py
@@ -2,11 +2,17 @@
 
 import pandas as pd
 
+
 def inverse_series(s):
-    return pd.Series()
+    return pd.Series(s.index, index=s.values)
+
 
 def main():
-    return
+    d = {2001: "Bush", 2005: "Bush", 2009: "Obama", 2013: "Obama", 2017: "Trump"}
+    s = pd.Series(d, name="Presidents")
+    inversed = inverse_series(s)
+    print(inversed)
+
 
 if __name__ == "__main__":
     main()
