35530a9f991e80f3150553610323f1ac13a8a72e
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-28 22:14:41 +0300

    Part04-e03 solution

--- part04-e03_municipal_information/src/municipal_information.py
+++ part04-e03_municipal_information/src/municipal_information.py
@@ -2,8 +2,12 @@
 
 import pandas as pd
 
+
 def main():
-    return
+    df = pd.read_csv("src/municipal.tsv", sep="\t", header=0, quotechar='"')
+    print(f"Shape: {df.shape[0]}, {df.shape[1]}")
+    print("Columns:")
+    print("\n".join(str(col) for col in df.columns))
 
 
 if __name__ == "__main__":
