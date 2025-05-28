0d79029e0b289c86337226d81fefabef5efa7b05
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-28 12:34:27 +0300

    Part04-e01 solution

--- part04-e01_cities/src/cities.py
+++ part04-e01_cities/src/cities.py
@@ -2,11 +2,23 @@
 
 import pandas as pd
 
+
 def cities():
-    return None
-    
+    columns = ["Population", "Total area"]
+    index = ["Helsinki", "Espoo", "Tampere", "Vantaa", "Oulu"]
+    data = [
+        [643272, 715.48],
+        [279044, 528.03],
+        [231853, 689.59],
+        [223027, 240.35],
+        [201810, 3817.52],
+    ]
+    return pd.DataFrame(data, columns=columns, index=index)
+
+
 def main():
-    return
-    
+    print(cities())
+
+
 if __name__ == "__main__":
     main()
