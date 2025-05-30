06025015fb52ba729d648bbd38b4c4ede64bafc4
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-30 18:00:20 +0300

    Part04-e06 solution

--- part04-e06_growing_municipalities/src/growing_municipalities.py
+++ part04-e06_growing_municipalities/src/growing_municipalities.py
@@ -2,11 +2,17 @@
 
 import pandas as pd
 
+
 def growing_municipalities(df):
-    0.0
+    all_population = df.shape[0]
+    grows = df[df["Population change from the previous year, %"] > 0].shape[0]
+    return grows / all_population
+
 
 def main():
-    return
+    df = pd.read_csv("src/municipal.tsv", sep="\t")
+    print(f"Proportion of growing municipalities: {growing_municipalities(df):.1f}%")
+
 
 if __name__ == "__main__":
     main()
