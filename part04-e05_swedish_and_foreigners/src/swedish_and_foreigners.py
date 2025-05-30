8211a3ed6bd51b532e2ea8c5fcb7601f09ef8ccc
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-30 10:06:40 +0300

    Part04-e05 solution

--- part04-e05_swedish_and_foreigners/src/swedish_and_foreigners.py
+++ part04-e05_swedish_and_foreigners/src/swedish_and_foreigners.py
@@ -2,11 +2,24 @@
 
 import pandas as pd
 
+
 def swedish_and_foreigners():
-    return None
+    df = pd.read_csv("src/municipal.tsv", sep="\t", index_col=0)
+    df_munic = df.loc["Akaa":"Äänekoski"]
+    mask1 = df_munic["Share of Swedish-speakers of the population, %"] > 5.0
+    mask2 = df_munic["Share of foreign citizens of the population, %"] > 5.0
+    columns = [
+        "Population",
+        "Share of Swedish-speakers of the population, %",
+        "Share of foreign citizens of the population, %",
+    ]
+
+    return df_munic.loc[mask1 & mask2, columns]
+
 
 def main():
-    return
+    print(swedish_and_foreigners())
+
 
 if __name__ == "__main__":
     main()
