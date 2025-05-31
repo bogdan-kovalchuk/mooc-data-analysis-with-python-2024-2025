998c46363d7314fe46c2e960a1997fd0242cd972
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-31 20:24:04 +0300

    Part04-e09 solution

--- part04-e09_snow_depth/src/snow_depth.py
+++ part04-e09_snow_depth/src/snow_depth.py
@@ -2,11 +2,15 @@
 
 import pandas as pd
 
+
 def snow_depth():
-    return 0.0
+    df = pd.read_csv("src/kumpula-weather-2017.csv")
+    return df.loc[:, "Snow depth (cm)"].max()
+
 
 def main():
-    return
+    print(f"Max snow depth: {snow_depth():.2f}")
+
 
 if __name__ == "__main__":
     main()
