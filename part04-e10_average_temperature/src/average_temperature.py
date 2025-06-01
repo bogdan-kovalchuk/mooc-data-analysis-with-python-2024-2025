c70112d01141cf9615033c0765b7fbe3e56a3f43
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-06-01 10:20:19 +0300

    Part04-e10 solution

--- part04-e10_average_temperature/src/average_temperature.py
+++ part04-e10_average_temperature/src/average_temperature.py
@@ -2,11 +2,15 @@
 
 import pandas as pd
 
+
 def average_temperature():
-    return 0.0
+    df = pd.read_csv("src/kumpula-weather-2017.csv")
+    return df[df["m"] == 7]["Air temperature (degC)"].mean()
+
 
 def main():
-    return
+    print(f"Average temperature in July: {average_temperature():.2f}")
+
 
 if __name__ == "__main__":
     main()
