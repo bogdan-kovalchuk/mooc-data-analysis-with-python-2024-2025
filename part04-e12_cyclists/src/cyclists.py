2a5a3275b129ebc575525f56f1eece98c792d0f8
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-06-02 09:37:12 +0300

    Part04-e12 solution

--- part04-e12_cyclists/src/cyclists.py
+++ part04-e12_cyclists/src/cyclists.py
@@ -2,12 +2,17 @@
 
 import pandas as pd
 
+
 def cyclists():
-    return None
+    df = pd.read_csv("src/Helsingin_pyorailijamaarat.csv", sep=";")
+    df.dropna(axis=0, how="all", inplace=True)
+    df.dropna(axis=1, how="all", inplace=True)
+    return df
 
 
 def main():
-    return
-    
+    print(cyclists().head())
+
+
 if __name__ == "__main__":
     main()
