f19cadbe6283c3539100dcd466b2979aac70007d
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-06-02 19:36:18 +0300

    Part04-e14 solution

--- part04-e14_special_missing_values/src/special_missing_values.py
+++ part04-e14_special_missing_values/src/special_missing_values.py
@@ -3,11 +3,17 @@
 import pandas as pd
 import numpy as np
 
+
 def special_missing_values():
-    return None
+    df = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep="\t")
+    df.replace(["New", "Re"], None, inplace=True)
+    df["LW"] = pd.to_numeric(df["LW"])
+    return df[df["Pos"] > df["LW"]]
+
 
 def main():
-    return
+    print(special_missing_values())
+
 
 if __name__ == "__main__":
     main()
