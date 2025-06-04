26d90497e8acb4f20c1cea3a02f7c7cb2336b71d
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-06-04 21:19:19 +0300

    Part04-e16 solution

--- part04-e16_split_date/src/split_date.py
+++ part04-e16_split_date/src/split_date.py
@@ -5,10 +5,52 @@ import numpy as np
 
 
 def split_date():
-    return None
+    df = (
+        pd.read_csv("src/Helsingin_pyorailijamaarat.csv", sep=";")
+        .dropna(how="all")
+        .dropna(axis=1, how="all")
+    )
+
+    date_parts = df["Päivämäärä"].str.extract(
+        r"(?P<Weekday>\w+)\s(?P<Day>\d{1,2})\s(?P<Month>\w+)\s(?P<Year>\d{4})\s(?P<Hour>\d{1,2}):\d{2}"
+    )
+
+    weekday_map = {
+        "ma": "Mon",
+        "ti": "Tue",
+        "ke": "Wed",
+        "to": "Thu",
+        "pe": "Fri",
+        "la": "Sat",
+        "su": "Sun",
+    }
+
+    month_map = {
+        "tammi": 1,
+        "helmi": 2,
+        "maalis": 3,
+        "huhti": 4,
+        "touko": 5,
+        "kesä": 6,
+        "heinä": 7,
+        "elo": 8,
+        "syys": 9,
+        "loka": 10,
+        "marras": 11,
+        "joulu": 12,
+    }
+
+    date_parts["Weekday"] = date_parts["Weekday"].map(weekday_map)
+    date_parts["Month"] = date_parts["Month"].map(month_map)
+
+    date_parts = date_parts.astype({"Day": int, "Month": int, "Year": int, "Hour": int})
+
+    return date_parts
+
 
 def main():
-    return
-       
+    print(split_date())
+
+
 if __name__ == "__main__":
     main()
