1e6b3b5312372f3a621a62ba4063305bb15c279d
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-06-10 13:57:43 +0300

    part05-e03 solution

--- part05-e03_top_bands/src/top_bands.py
+++ part05-e03_top_bands/src/top_bands.py
@@ -2,11 +2,17 @@
 
 import pandas as pd
 
+
 def top_bands():
-    return pd.DataFrame()
+    bands = pd.read_csv("src/bands.tsv", sep="\t")
+    top40 = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep="\t")
+    bands["Band"] = bands["Band"].str.upper()
+    return pd.merge(top40, bands, left_on="Artist", right_on="Band")
+
 
 def main():
-    return
+    print(top_bands())
+
 
 if __name__ == "__main__":
     main()
