0aca4a7f34050fc2f4e39301686f4ac6ed2607d3
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-08-12 16:15:43 +0300

    part05-e13 solution

--- part05-e13_cycling_weather_continues/src/cycling_weather_continues.py
+++ part05-e13_cycling_weather_continues/src/cycling_weather_continues.py
@@ -1,71 +1,72 @@
 #!/usr/bin/env python3
+
 import pandas as pd
-from sklearn import linear_model
+from sklearn.linear_model import LinearRegression
+
+days = dict(zip("ma ti ke to pe la su".split(), "Mon Tue Wed Thu Fri Sat Sun".split()))
+months = dict(
+    zip(
+        "tammi helmi maalis huhti touko kesä heinä elo syys loka marras joulu".split(),
+        range(1, 13),
+    )
+)
 
 
 def split_date(df):
-    date_parts = df["Päivämäärä"].str.extract(
-        r"(?P<Weekday>\w+)\s(?P<Day>\d{1,2})\s(?P<Month>\w+)\s(?P<Year>\d{4})\s(?P<Hour>\d{1,2}):\d{2}"
+    d = df["Päivämäärä"].str.split(expand=True)
+    d.columns = ["Weekday", "Day", "Month", "Year", "Hour"]
+
+    hourmin = d["Hour"].str.split(":", expand=True)
+    d["Hour"] = hourmin.iloc[:, 0]
+
+    d["Weekday"] = d["Weekday"].map(days)
+    d["Month"] = d["Month"].map(months)
+
+    d = d.astype(
+        {"Weekday": object, "Day": int, "Month": int, "Year": int, "Hour": int}
     )
+    return d
 
-    weekday_map = {
-        "ma": "Mon",
-        "ti": "Tue",
-        "ke": "Wed",
-        "to": "Thu",
-        "pe": "Fri",
-        "la": "Sat",
-        "su": "Sun",
-    }
 
-    month_map = {
-        "tammi": 1,
-        "helmi": 2,
-        "maalis": 3,
-        "huhti": 4,
-        "touko": 5,
-        "kesä": 6,
-        "heinä": 7,
-        "elo": 8,
-        "syys": 9,
-        "loka": 10,
-        "marras": 11,
-        "joulu": 12,
-    }
-
-    date_parts["Weekday"] = date_parts["Weekday"].map(weekday_map)
-    date_parts["Month"] = date_parts["Month"].map(month_map)
-
-    date_parts = date_parts.astype({"Day": int, "Month": int, "Year": int, "Hour": int})
-
+def split_date_continues():
+    df = pd.read_csv("src/Helsingin_pyorailijamaarat.csv", sep=";")
+    df = df.dropna(axis=0, how="all").dropna(axis=1, how="all")
+    d = split_date(df)
     df = df.drop("Päivämäärä", axis=1)
-    return pd.concat([date_parts, df], axis=1)
+    return pd.concat([d, df], axis=1)
 
 
 def cycling_weather():
-    df1 = pd.read_csv("src/kumpula-weather-2017.csv", sep=",").dropna(how="all").dropna(axis=1, how="all")
-    df2 = pd.read_csv("src/Helsingin_pyorailijamaarat.csv", sep=";").dropna(how="all").dropna(axis=1, how="all")
-    df2 = split_date(df2)
-    marged = pd.merge(df1, df2, left_on=["Year", "m", "d"], right_on=["Year", "Month", "Day"])
-    return marged.drop(["m", "d", "Time", "Time zone"], axis=1).ffill()
+    wh = pd.read_csv("src/kumpula-weather-2017.csv")
+    bike = split_date_continues()
+    bike = bike[bike.Year == 2017]
+    bike = bike.groupby(["Year", "Month", "Day"]).sum()
+    bike = bike.reset_index().drop(["Hour"], axis=1)
+    result = wh.merge(
+        bike, left_on=["Year", "m", "d"], right_on=["Year", "Month", "Day"]
+    )
+
+    return result.drop(["m", "d", "Time", "Time zone"], axis=1)
 
 
-def cycling_weather_continues(station=None):
+def cycling_weather_continues(station):
     df = cycling_weather()
+    df = df.fillna(method="ffill")
     X = df[["Precipitation amount (mm)", "Snow depth (cm)", "Air temperature (degC)"]]
-    y = df[[station]]
-    model = linear_model.LinearRegression(fit_intercept=True)
-    model.fit(X, y)
-    return (model.coef_, model.score(X, y))
+    y = df[station]
+    reg = LinearRegression()
+    reg.fit(X, y)
+    score = reg.score(X, y)
+    return reg.coef_, score
 
 
 def main():
-    station = input("Measuring station: ")
-    coefficients, score = cycling_weather_continues(station)
-    c1, c2, c3 = coefficients[0]
-    print(f"Regression coefficient for variable 'precipitation': {c1:.1f}")
-    print(f"Regression coefficient for variable 'snow depth': {c2:.1f}")
-    print(f"Regression coefficient for variable 'temperature': {c3:.1f}")
+    station = "Baana"
+    coef, score = cycling_weather_continues(station)
+    print("Measuring station:", station)
+    print(f"Regression coefficient for variable 'precipitation': {coef[0]:.1f}")
+    print(f"Regression coefficient for variable 'snow depth': {coef[1]:.1f}")
+    print(f"Regression coefficient for variable 'temperature': {coef[2]:.1f}")
     print(f"Score: {score:.2f}")
 
 
