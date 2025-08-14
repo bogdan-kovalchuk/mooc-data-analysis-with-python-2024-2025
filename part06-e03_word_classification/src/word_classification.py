45e8c22feedaea81bf4f33bd2e27433649c316c9
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-08-14 15:01:47 +0300

    part06-e03 solution

--- part06-e03_word_classification/src/word_classification.py
+++ part06-e03_word_classification/src/word_classification.py
@@ -7,7 +7,7 @@ from lxml import etree
 import numpy as np
 
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, KFold
 from sklearn import model_selection
 
 alphabet = "abcdefghijklmnopqrstuvwxyzäö-"
@@ -36,23 +36,32 @@ def load_finnish():
 def load_english():
     with open("src/words", encoding="utf-8") as data:
         lines = map(lambda s: s.rstrip(), data.readlines())
-    return lines
+    return list(lines)
 
 
-def get_features(a):
-    return np.array([[]])
+def get_features(features: np.array):
+    word_counters = [Counter(word) for word in features]
+    return np.array([[counts.get(ch, 0) for ch in alphabet] for counts in word_counters])
 
 
-def contains_valid_chars(s):
-    return True
+def contains_valid_chars(string: str):
+    return all(char in alphabet_set for char in string)
 
 
 def get_features_and_labels():
-    return np.array([[]]), np.array([])
+    fin = [w.lower() for w in load_finnish() if contains_valid_chars(w.lower())]
+    eng = [w.lower() for w in load_english() if contains_valid_chars(w[0]) and contains_valid_chars(w.lower())]
+    X = get_features(fin + eng)
+    y = np.array([0] * len(fin) + [1] * len(eng))
+    return X, y
 
 
 def word_classification():
-    return []
+    model = MultinomialNB()
+    X, y = get_features_and_labels()
+    kf = KFold(n_splits=5, shuffle=True, random_state=0)
+    scores = cross_val_score(model, X, y, cv=kf, scoring="accuracy")
+    return list(scores)
 
 
 def main():
