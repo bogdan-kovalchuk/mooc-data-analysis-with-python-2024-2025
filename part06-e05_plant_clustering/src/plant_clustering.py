8ebf5c56d40ee9d4f07a53072077be8ddc67bac7
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-08-21 16:15:49 +0300

    part06-e05 solution

--- part06-e05_plant_clustering/src/plant_clustering.py
+++ part06-e05_plant_clustering/src/plant_clustering.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python3
 
+import sklearn
 import scipy
+
+from sklearn.datasets import load_iris
+from sklearn.cluster import KMeans
 
 
 def find_permutation(n_clusters, real_labels, labels):
-    permutation=[]
+    permutation = []
     for i in range(n_clusters):
         idx = labels == i
         # Choose the most common label among data points in the cluster
-        new_label=scipy.stats.mode(real_labels[idx])[0]
+        new_label = scipy.stats.mode(real_labels[idx])[0]
         permutation.append(new_label)
     return permutation
 
+
 def plant_clustering():
-    return 0.0
+    X, y = load_iris(return_X_y=True)
+    model = KMeans(n_clusters=3, n_init=10, random_state=0)
+    model.fit(X)
+    permutation = find_permutation(3, y, model.labels_)
+    return sklearn.metrics.accuracy_score(y, [permutation[label] for label in model.labels_])
+
 
 def main():
     print(plant_clustering())
+
 
 if __name__ == "__main__":
     main()
