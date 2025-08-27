70c533664829773f01677a8fe7f75cbd44dbb30c
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-08-27 15:48:27 +0300

    part06-e07 solution

--- part06-e07_binding_sites/src/binding_sites.py
+++ part06-e07_binding_sites/src/binding_sites.py
@@ -9,32 +9,52 @@ from sklearn.metrics import pairwise_distances
 from matplotlib import pyplot as plt
 
 import seaborn as sns
+
 sns.set(color_codes=True)
+import scipy
 import scipy.spatial as sp
 import scipy.cluster.hierarchy as hc
 
+
+def find_permutation(n_clusters, real_labels, labels):
+    permutation = []
+    for i in range(n_clusters):
+        idx = labels == i
+        new_label = scipy.stats.mode(real_labels[idx])[0]
+        permutation.append(new_label)
+    return permutation
+
+
 def toint(x):
-    return 0
+    return {"A": 0, "C": 1, "G": 2, "T": 3}[x]
+
 
 def get_features_and_labels(filename):
-    return (np.array([[]]), np.array([]))
+    df = pd.read_csv(filename, sep="\t")
+    X = df["X"].apply(list).apply(pd.Series).applymap(toint).to_numpy()
+    y = df["y"].to_numpy()
+    return X, y
 
-def plot(distances, method='average', affinity='euclidean'):
-    mylinkage = hc.linkage(sp.distance.squareform(distances), method=method)
-    g=sns.clustermap(distances, row_linkage=mylinkage, col_linkage=mylinkage )
-    g.fig.suptitle(f"Hierarchical clustering using {method} linkage and {affinity} metric")
-    plt.show()
 
 def cluster_euclidean(filename):
-    return 0.0
+    X, y = get_features_and_labels(filename)
+    clustering = AgglomerativeClustering(2, linkage="average", metric="euclidean")
+    y_fitted = clustering.fit_predict(X)
+    return accuracy_score(y, y_fitted)  # 0.9865
+
 
 def cluster_hamming(filename):
-    return 0.0
+    X, y = get_features_and_labels(filename)
+    clustering = AgglomerativeClustering(2, linkage="average", metric="precomputed")
+    dist_matrix = pairwise_distances(X, metric="hamming")
+    y_fitted = 1 - clustering.fit_predict(dist_matrix)
+    return accuracy_score(y, y_fitted)  # 0.9975
 
 
 def main():
     print("Accuracy score with Euclidean metric is", cluster_euclidean("src/data.seq"))
     print("Accuracy score with Hamming metric is", cluster_hamming("src/data.seq"))
+
 
 if __name__ == "__main__":
     main()
